335d0d832e4b6f28a9f7221abaec0eff7d68e584
Tyler Caceres <tyler.caceres@gmail.com>
2020-02-11 20:21:27 -0500

    commit 2
    commit 2

--- src/Example1.jsx
+++ src/Example1.jsx
@@ -7,15 +7,26 @@ const Example1 = ({ data }) => {
     <div className="outer-box">
       <div className="header-box">Example 1 - SCSS</div>
       <div className="content-box">
-        <ul className="">
-          {data.map((person, i) => {
-            return (
-              <li key={i}>
-                <pre>{person}</pre>
-              </li>
-            );
-          })}
-        </ul>
+        <table className="main-table">
+          <thead>
+            <tr>
+              {Object.keys(data[0]).map((field, i) => {
+                return <th key={i}>{field}</th>;
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, i) => {
+              return (
+                <tr key={i}>
+                  {Object.values(row).map((field, j) => {
+                    return <td key={j}>{field}</td>;
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
